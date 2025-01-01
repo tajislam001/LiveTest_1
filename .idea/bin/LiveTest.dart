27c90793177621b4d5c24293073318cc5e14186d
tajislam001 <taj.islam@gmail.com>
2025-01-01 23:18:26 +0600

    live test 01

--- .idea/bin/LiveTest.dart
+++ .idea/bin/LiveTest.dart
@@ -6,13 +6,13 @@ void main() {
 }
 
 void triangle() {
-  print("Enter the base of the triangle:");
+  print("Enter the base:");
   double base = double.parse(stdin.readLineSync()!);
 
-  print("Enter the height of the triangle:");
+  print("Enter the height:");
   double height = double.parse(stdin.readLineSync()!);
 
   double area = 0.5 * base * height;
 
-  print("The area of the triangle is: $area");
+  print("The area of the triangle: $area");
 }
